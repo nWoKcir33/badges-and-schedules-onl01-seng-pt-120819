c53fff7b26fa9b340d0eac5ef2c647b0458b6a2e
Rick Lim <rick.jason.a.lim@gmail.com>
2019-12-16 23:38:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -22,5 +22,12 @@ end
 return arrays
 end
 
-def printer
+def printer(array)
+  batch_badge_creator(array).each do |badge|
+    puts badge
+  end
+  assign_rooms(array).each do |assignment|
+    puts assignment
+  end
+end
   
